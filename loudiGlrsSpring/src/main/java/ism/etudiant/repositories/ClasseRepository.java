bbf590c5d798789565eab0018cb44a5b7444dd8d
fortunatglondel <136086165+fortunatglondel@users.noreply.github.com>
2023-06-09 12:29:48 +0000

    Add files via upload

---
+++ loudiGlrsSpring/src/main/java/ism/etudiant/repositories/ClasseRepository.java
@@ -0,0 +1,22 @@
+package ism.etudiant.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ism.etudiant.entities.Classe;
+import ism.etudiant.entities.Etudiant;
+
+import java.util.List;
+
+public interface ClasseRepository extends JpaRepository<Classe,Long> {
+
+
+
+   //=================Mon Repo de classe Save List
+    
+
+
+
+
+}
